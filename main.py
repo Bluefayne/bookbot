c50b98aa358ce83821892cbc8fc549d38bc17797
Bluefayne <bluefayne4@gmail.com>
2024-06-21 21:14:21 +1000

    add get_book_test, add get_num_words, add get_char_count

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    char_count = get_char_count(text)
+    print(char_count)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_count(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+main()        
